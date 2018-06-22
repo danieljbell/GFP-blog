73ab1cc1a38a3b41543d9bb1ca39113d80cbc356
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-06-22 16:34:25 -0400

    fixing link bug

--- wp-content/themes/gfp/front-page.php
+++ wp-content/themes/gfp/front-page.php
@@ -1,17 +1,5 @@
 <?php get_header(); ?>
 
-<?php $args = array(
-  'type'            => 'yearly',
-  'limit'           => '',
-  'format'          => 'link', 
-  'before'          => '',
-  'after'           => '',
-  'show_post_count' => false,
-  'echo'            => 1,
-  'order'           => 'DESC',
-        'post_type'     => 'post'
-);
-wp_get_archives( $args ); ?>
 
 <section class="post-listing--full-details">
   
@@ -38,7 +26,7 @@ wp_get_archives( $args ); ?>
         <div class="single-current-promo-content">
           <h4 class="single-current-promo-headline"><?php the_field('current_promo_headline', 'option'); ?></h4>
           <p class="single-current-promo-copy"><?php the_field('current_promo_copy', 'option'); ?></p>
-          <a href="<?php the_field('current_promo_button_link'); ?>"><?php the_field('current_promo_button_text', 'option'); ?></a>
+          <a href="<?php the_field('current_promo_button_link', 'option'); ?>"><?php the_field('current_promo_button_text', 'option'); ?></a>
         </div>
       </div>
     </div>
