49e52d3f2d5035c0e93652aa97bde3e8f037d3a0
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-06-26 14:05:28 -0400

    revert to pull from admin

--- wp-content/themes/gfp/header.php
+++ wp-content/themes/gfp/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-  <base href="https://blog.greenfarmparts.com">
+  <base href="<?php echo site_url(); ?>">
   <meta charset="<?php bloginfo('charset'); ?>">
   <title><?php wp_title(''); ?></title>
   <meta http-equiv="x-ua-compatible" content="ie=edge">
