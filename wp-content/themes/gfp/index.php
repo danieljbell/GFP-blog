614659a8f2f123382bd5c402993b077ffef925d4
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-06-26 16:35:31 -0400

    author headline

--- wp-content/themes/gfp/index.php
+++ wp-content/themes/gfp/index.php
@@ -17,6 +17,10 @@
         $queried_object = get_queried_object();
         $page_title = 'Content Tagged As "' . $queried_object->name . '"';
       }
+      if (is_author()) {
+        $queried_object = get_queried_object();
+        $page_title = 'Content Written By ' . $queried_object->display_name . '';
+      }
     ?>
     <h1><?php echo $page_title ?></h1>
     <h2><?php echo $wp_query->post_count . ' ' . $page_subtitle; ?></h2>
