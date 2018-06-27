c64d71a88580c3502d06ce2a13ead57fe27874fb
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-06-27 16:30:15 -0400

    chugging thru header

--- wp-content/themes/gfp/partials/display-site-header.php
+++ wp-content/themes/gfp/partials/display-site-header.php
@@ -8,6 +8,11 @@
     </div>
     <nav>
       <ul class="navigation--level-zero">
+        <li class="mobile-search-container">
+          <form action="https://greenfarmparts.com/searchresults.asp" method="get" name="SearchBoxForm" id="mobileSearchBar">
+            <input placeholder="Search..." type="search" name="Search" class="mobile-search ui-autocomplete-input" autocomplete="off" role="textbox" aria-autocomplete="list" aria-haspopup="true">
+          </form>
+        </li>
         <li>
           <button class="navigation--button">Shop By Part</button>
           <?php
@@ -25,6 +30,12 @@
               'menu_class' => 'navigation--level-one'
             ) );
           ?>
+        </li>
+        <li class="cart-container">
+          <a href="/shoppingcart.asp">
+            <span class="mobile-only">Shopping Cart</span>
+            <img src="https://greenfarmparts.com/v/vspfiles/templates/gfp-test/img/cart-icon.jpg" style="display: inline-block; vertical-align: middle; border-radius: 50%; max-width: 40px;">
+          </a>
         </li>
       </ul>
     </nav>
