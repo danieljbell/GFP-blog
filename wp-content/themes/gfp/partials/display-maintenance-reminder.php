13a621581c486af1943e703fcdabbbedb8ede540
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-07-31 10:21:54 -0400

    adding a bunch of models

--- wp-content/themes/gfp/partials/display-maintenance-reminder.php
+++ wp-content/themes/gfp/partials/display-maintenance-reminder.php
@@ -83,25 +83,25 @@ foreach (get_the_tags() as $tag) {
         <img src="<?php echo $model_image; ?>" alt="John Deere <?php echo $model_number . ' ' . $model_name; ?>">
       </div>
       <?php
-        $model_modifiers = get_field('model_modifiers');
-        if ($model_modifiers) {
-          echo '<h4>Looking for a different model in this series?</h4><p style="margin-bottom: 0.5rem;">Browse other models in this series below</p>';
-          echo '<select id="modelModifiers" style="width: 100%;">';
-            echo '<option selected disabled>Choose Different Model in this Series</option>';
-          foreach ($model_modifiers as $post) {
-            setup_postdata($post);
-            // global $post;
-            $post_slug = $post->post_name;
-            $stripped_title = str_replace('John Deere ', '', str_replace('Maintenance Sheet', '', get_the_title()));
-            if (('/' . $post_slug . '/') == $_SERVER['REQUEST_URI']) {
-              echo '<option selected value="' . $post_slug . '">' . $stripped_title . '</option>';
-            } else {
-              echo '<option value="' . $post_slug . '">' . $stripped_title . '</option>';
-            }
-          }
-          wp_reset_postdata();
-          echo '</select>';
-        }
+        // $model_modifiers = get_field('model_modifiers');
+        // if ($model_modifiers) {
+        //   echo '<h4>Looking for a different model in this series?</h4><p style="margin-bottom: 0.5rem;">Browse other models in this series below</p>';
+        //   echo '<select id="modelModifiers" style="width: 100%;">';
+        //     echo '<option selected disabled>Choose Different Model in this Series</option>';
+        //   foreach ($model_modifiers as $post) {
+        //     setup_postdata($post);
+        //     // global $post;
+        //     $post_slug = $post->post_name;
+        //     $stripped_title = str_replace('John Deere ', '', str_replace('Maintenance Sheet', '', get_the_title()));
+        //     if (('/' . $post_slug . '/') == $_SERVER['REQUEST_URI']) {
+        //       echo '<option selected value="' . $post_slug . '">' . $stripped_title . '</option>';
+        //     } else {
+        //       echo '<option value="' . $post_slug . '">' . $stripped_title . '</option>';
+        //     }
+        //   }
+        //   wp_reset_postdata();
+        //   echo '</select>';
+        // }
       ?>
 
       <?php if ($maintenance_kit) : ?>
@@ -532,10 +532,86 @@ foreach (get_the_tags() as $tag) {
           <div class="mc-field-group">
             <label for="mce-MODEL">Model</label>
             <select name="MODEL" class="required" id="mce-MODEL">
-              <option value=""></option>
-              <option value="E100">E100</option>
-              <option value="E110">E110</option>
-              <option value="E120">E120</option>
+            <option value="" disabled selected>Choose Your Model</option>
+            <option value="115">115</option>
+            <option value="125">125</option>
+            <option value="L100">L100</option>
+            <option value="L110">L110</option>
+            <option value="LA155">LA155</option>
+            <option value="D100">D100</option>
+            <option value="D105">D105</option>
+            <option value="D110">D110</option>
+            <option value="D125">D125</option>
+            <option value="D130">D130</option>
+            <option value="D140">D140</option>
+            <option value="D170">D170</option>
+            <option value="E100">E100</option>
+            <option value="E110">E110</option>
+            <option value="E120">E120</option>
+            <option value="E130">E130</option>
+            <option value="E140">E140</option>
+            <option value="E150">E150</option>
+            <option value="E160">E160</option>
+            <option value="E170">E170</option>
+            <option value="E180">E180</option>
+            <option value="S240">S240</option>
+            <option value="X300R">X300R</option>
+            <option value="X304">X304</option>
+            <option value="X310">X310</option>
+            <option value="X324">X324</option>
+            <option value="X330">X330</option>
+            <option value="X340">X340</option>
+            <option value="X350">X350</option>
+            <option value="X350R">X350R</option>
+            <option value="X354">X354</option>
+            <option value="X360">X360</option>
+            <option value="X370">X370</option>
+            <option value="X380">X380</option>
+            <option value="X384">X384</option>
+            <option value="X384">X384</option>
+            <option value="X390">X390</option>
+            <option value="X394">X394</option>
+            <option value="X520">X520</option>
+            <option value="X534">X534</option>
+            <option value="X540">X540</option>
+            <option value="X570">X570</option>
+            <option value="X580">X580</option>
+            <option value="X580">X580</option>
+            <option value="X584">X584</option>
+            <option value="X590">X590</option>
+            <option value="X710">X710</option>
+            <option value="X728">X728</option>
+            <option value="X730">X730</option>
+            <option value="X734">X734</option>
+            <option value="X738">X738</option>
+            <option value="X739">X739</option>
+            <option value="X739">X739</option>
+            <option value="X750">X750</option>
+            <option value="X754">X754</option>
+            <option value="X758">X758</option>
+            <option value="Z225">Z225</option>
+            <option value="Z235">Z235</option>
+            <option value="Z255">Z255</option>
+            <option value="Z335E">Z335E</option>
+            <option value="Z335M">Z335M</option>
+            <option value="Z345M">Z345M</option>
+            <option value="Z345R">Z345R</option>
+            <option value="Z355E">Z355E</option>
+            <option value="Z355R">Z355R</option>
+            <option value="Z375R">Z375R</option>
+            <option value="Z425">Z425</option>
+            <option value="Z435">Z435</option>
+            <option value="Z445">Z445</option>
+            <option value="Z465">Z465</option>
+            <option value="Z525E">Z525E</option>
+            <option value="Z535M">Z535M</option>
+            <option value="Z535R">Z535R</option>
+            <option value="Z540M">Z540M</option>
+            <option value="Z540R">Z540R</option>
+            <option value="Z625">Z625</option>
+            <option value="Z645">Z645</option>
+            <option value="Z655">Z655</option>
+            <option value="Z665">Z665</option>
             </select>
           </div>
           <div class="mc-field-group size1of2">
@@ -545,19 +621,19 @@ foreach (get_the_tags() as $tag) {
           <div class="mc-field-group">
             <label for="mce-MOWING_TIM">Mowing Duration </label>
             <select name="MOWING_TIM" class="" id="mce-MOWING_TIM">
-              <option value=""></option>
+              <option value="" selected disabled>How long does it take to mow?</option>
               <option value="Small-Medium Yard (Less than 1.5 hours)">Small-Medium Yard (Less than 1.5 hours)</option>
               <option value="Large Property (More than 1.5 hours)">Large Property (More than 1.5 hours)</option>
             </select>
           </div>
           <div class="mc-field-group input-group">
             <ul>
-              <li><input type="checkbox" value="1" name="group[141][1]" id="mce-group[141]-141-0"><label for="mce-group[141]-141-0">Give me maintenance reminders via email communications.</label></li>
+              <li><input type="checkbox" value="1" name="group[141][1]" id="mce-group[141]-141-0" checked><label for="mce-group[141]-141-0">Give me maintenance reminders via email communications.</label></li>
             </ul>
           </div>
           <div class="mc-field-group input-group">
             <ul>
-              <li><input type="checkbox" value="2" name="group[145][2]" id="mce-group[145]-145-0"><label for="mce-group[145]-145-0">I consent to having my maintenance reminder engagement tracked and analyzed.</label></li>
+              <li><input type="checkbox" value="2" name="group[145][2]" id="mce-group[145]-145-0" checked><label for="mce-group[145]-145-0">I consent to having my maintenance reminder engagement tracked and analyzed.</label></li>
             </ul>
           </div>
           <div id="mce-responses" class="clear">
