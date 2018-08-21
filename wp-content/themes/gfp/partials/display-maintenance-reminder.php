742d293dbbfb05fc5907c0df802b791b87a7df66
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-08-21 16:03:32 -0400

    every for single hour

--- wp-content/themes/gfp/partials/display-maintenance-reminder.php
+++ wp-content/themes/gfp/partials/display-maintenance-reminder.php
@@ -260,7 +260,7 @@ foreach (get_the_tags() as $tag) {
               </td>
               <td data-header="Part Number"><?php echo $product_link; ?></td>
               <td data-header="Quantity"><?php echo get_sub_field('quantity'); ?></td>
-              <td data-header="Hour Intervals"><?php print_r($interval_hour);// if (count(explode(',', $interval_hour)) < 2) echo 'Every '; echo $interval_hour;  ?></td>
+              <td data-header="Hour Intervals"><?php if (count(explode(',', $interval_hour)) < 2) echo 'Every '; echo $interval_hour;  ?></td>
               <td><?php echo $available_online; ?></td>
             </tr>
         <?php endwhile; ?>
