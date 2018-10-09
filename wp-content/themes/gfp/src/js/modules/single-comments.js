fe5acdd696c0605d7ef9fc37b1ab0fc6e8581573
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-10-09 09:46:09 -0400

    cleaning up img on product pages

--- wp-content/themes/gfp/src/js/modules/single-comments.js
+++ wp-content/themes/gfp/src/js/modules/single-comments.js
@@ -1,33 +1,33 @@
 (function() {
 
-  var commentsContainer = document.querySelector('.single-comments');
-  var commentsReply = document.querySelector('#respond');
-  var commentForm = document.querySelector('#commentform');
+  // var commentsContainer = document.querySelector('.single-comments');
+  // var commentsReply = document.querySelector('#respond');
+  // var commentForm = document.querySelector('#commentform');
 
 
-  if (commentsReply) {
-    var button = document.createElement("button");
-    button.innerHTML = 'Add Comment';
-    button.id = 'addComment';
+  // if (commentsReply) {
+  //   var button = document.createElement("button");
+  //   button.innerHTML = 'Add Comment';
+  //   button.id = 'addComment';
 
-    commentsReply.style.visibility = 'hidden';
-    commentsReply.style.maxHeight = '0px';
-    commentsReply.style.padding = '0px';
-    commentsReply.style.marginTop = '0px';
+  //   commentsReply.style.visibility = 'hidden';
+  //   commentsReply.style.maxHeight = '0px';
+  //   commentsReply.style.padding = '0px';
+  //   commentsReply.style.marginTop = '0px';
 
-    commentsContainer.appendChild(button);
+  //   commentsContainer.appendChild(button);
 
-    document.querySelector('#addComment').addEventListener('click', function(e) {
-      e.preventDefault();
-      var self = e.target;
-      self.style.opacity = '0';
-      commentsReply.style.visibility = 'visible';
-      commentsReply.style.maxHeight = '1000px';
-      commentsReply.style.padding = '1rem';
-      commentsReply.style.marginTop = '3rem';
-    })
+  //   document.querySelector('#addComment').addEventListener('click', function(e) {
+  //     e.preventDefault();
+  //     var self = e.target;
+  //     self.style.opacity = '0';
+  //     commentsReply.style.visibility = 'visible';
+  //     commentsReply.style.maxHeight = '1000px';
+  //     commentsReply.style.padding = '1rem';
+  //     commentsReply.style.marginTop = '3rem';
+  //   })
 
-  }
+  // }
 
 
 })();
