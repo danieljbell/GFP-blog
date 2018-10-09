8a3e8fa3719f81e337140975a9481f8df47d7f6f
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-10-09 13:44:56 -0400

    update url

--- wp-content/themes/gfp/partials/display-search.php
+++ wp-content/themes/gfp/partials/display-search.php
@@ -1,6 +1,6 @@
 <div class="global-search-bar">
   <div class="site-width">
-    <form role="search" method="get" action="//localhost:3000/">
+    <form role="search" method="get" action="/">
       <div>
         <label class="screen-reader-text visually-hidden" for="s">Search for:</label>
         <input type="text" value="" name="s" id="s" placeholder="Search by Part Number or Model Number" autocomplete="off">
