3993666cc1c0e5371b5076fa670f3c34b6c13dfa
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-10-12 10:29:45 -0400

    let\s try it

---
+++ wp-content/mu-plugins/custom-post-types.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * Plugin Name:  GFP Custom Post Types
+ * Description:  Create custom post types to manage sitewide promotions and more
+ * Version:      1.0.0
+ * Author:       Daniel Bell
+ * Author URI:   https://github.com/danieljbell
+ *
+ * @link         https://github.com/danieljbell
+ * @package      WordPress
+ * @author       Daniel Bell
+ * @version      1.0.0
+ */
