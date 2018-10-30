920adf985a6737bf6660a5bd53055ebc3621b5dc
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-10-30 16:58:32 -0400

    howTo schema for troubleshooting

--- wp-content/themes/gfp/partials/display-troubleshooting.php
+++ wp-content/themes/gfp/partials/display-troubleshooting.php
@@ -142,3 +142,329 @@
 
   </div>
 </section>
+
+<script type="application/ld+json">
+  {
+    "@context": "http://schema.org",
+    "@graph": [
+      {
+        "@context": "https://schema.org",
+        "@type": "HowTo",
+        "name": "<?php echo get_the_title(); ?>",
+        "step": [
+          {
+            "@type": "HowToSection",
+            "name": "Common Engine Problems for <?php echo get_the_title(); ?>",
+            "position": "1",
+            "itemListElement": [
+              <?php
+                if (have_rows('engine_problems')) :
+                  $i = 1;
+                  while(have_rows('engine_problems')) :
+                    the_row();
+              ?>
+                {
+                  "@type": "HowToStep",
+                  "position": "<?php echo $i; ?>",
+                  "itemListElement": [
+                    {
+                      "@type": "HowToDirection",
+                      "position": "1",
+                      "text": "<?php echo get_sub_field('problem'); ?>"
+                    },
+                    {
+                      "@type": "HowToTip",
+                      "position": "2",
+                      "text": "<?php echo preg_replace( "/\r|\n/", " ", trim(strip_tags( get_sub_field('solution') )) ); ?>"
+                    }
+                  ]
+                }<?php if ($i < count(get_field('engine_problems'))) { echo ','; } ?>
+              <?php $i++; endwhile; endif; ?>
+            ]
+          },
+          {
+            "@type": "HowToSection",
+            "name": "Common Electrical Problems for <?php echo get_the_title(); ?>",
+            "position": "2",
+            "itemListElement": [
+              <?php
+                if (have_rows('electrical_problems')) :
+                  $i = 1;
+                  while(have_rows('electrical_problems')) :
+                    the_row();
+              ?>
+                {
+                  "@type": "HowToStep",
+                  "position": "<?php echo $i; ?>",
+                  "itemListElement": [
+                    {
+                      "@type": "HowToDirection",
+                      "position": "1",
+                      "text": "<?php echo get_sub_field('problem'); ?>"
+                    },
+                    {
+                      "@type": "HowToTip",
+                      "position": "2",
+                      "text": "<?php echo preg_replace( "/\r|\n/", " ", trim(strip_tags( get_sub_field('solution') )) ); ?>"
+                    }
+                  ]
+                }<?php if ($i < count(get_field('electrical_problems'))) { echo ','; } ?>
+              <?php $i++; endwhile; endif; ?>
+            ]
+          },
+          {
+            "@type": "HowToSection",
+            "name": "Common Machine Problems for <?php echo get_the_title(); ?>",
+            "position": "3",
+            "itemListElement": [
+              <?php
+                if (have_rows('machine_problems')) :
+                  $i = 1;
+                  while(have_rows('machine_problems')) :
+                    the_row();
+              ?>
+                {
+                  "@type": "HowToStep",
+                  "position": "<?php echo $i; ?>",
+                  "itemListElement": [
+                    {
+                      "@type": "HowToDirection",
+                      "position": "1",
+                      "text": "<?php echo get_sub_field('problem'); ?>"
+                    },
+                    {
+                      "@type": "HowToTip",
+                      "position": "2",
+                      "text": "<?php echo preg_replace( "/\r|\n/", " ", trim(strip_tags( get_sub_field('solution') )) ); ?>"
+                    }
+                  ]
+                }<?php if ($i < count(get_field('machine_problems'))) { echo ','; } ?>
+              <?php $i++; endwhile; endif; ?>
+            ]
+          },
+          {
+            "@type": "HowToSection",
+            "name": "Common Mower Problems for <?php echo get_the_title(); ?>",
+            "position": "4",
+            "itemListElement": [
+              <?php
+                if (have_rows('mower_problems')) :
+                  $i = 1;
+                  while(have_rows('mower_problems')) :
+                    the_row();
+              ?>
+                {
+                  "@type": "HowToStep",
+                  "position": "<?php echo $i; ?>",
+                  "itemListElement": [
+                    {
+                      "@type": "HowToDirection",
+                      "position": "1",
+                      "text": "<?php echo get_sub_field('problem'); ?>"
+                    },
+                    {
+                      "@type": "HowToTip",
+                      "position": "2",
+                      "text": "<?php echo preg_replace( "/\r|\n/", " ", trim(strip_tags( get_sub_field('solution') )) ); ?>"
+                    }
+                  ]
+                }<?php if ($i < count(get_field('mower_problems'))) { echo ','; } ?>
+              <?php $i++; endwhile; endif; ?>
+            ]
+          }
+        ]
+      },
+      {
+        <?php $terms = get_the_terms($post->ID, 'category')[0]; ?>
+        "@context": "https://schema.org/",
+        "@type": "BreadcrumbList",
+        "itemListElement": [
+          {
+            "@type": "ListItem",
+            "position": "1",
+            "item": {
+              "name": "Home",
+              "@id": "<?php echo site_url(); ?>"
+            }
+          },
+          {
+            "@type": "ListItem",
+            "position": "2",
+            "item": {
+              "name": "<?php echo $terms->name; ?>",
+              "@id": "<?php echo site_url() . '/' . $terms->slug; ?>"
+            }
+          },
+          {
+            "@type": "ListItem",
+            "position": "3",
+            "item": {
+              "name": "<?php echo get_the_title(); ?>",
+              "@id": "<?php echo get_the_permalink(); ?>"
+            }
+          }
+        ]
+      }
+    ]
+  }
+</script>
+
+<?php print_r(get_the_terms($post->ID, 'category')); ?>
+
+<?php
+/*
+=========================
+<script type="application/json+ld"/>
+{
+  "@context":"http://schema.org",
+  "@type":"HowTo",
+  "name":"How To Add Your Website To Google Search Console",
+  "steps":
+  [
+  {
+    "@type":"HowToSection",
+    "name":"Preparation",
+                "position": "1",
+    "itemListElement":
+    [
+    {
+      "@type":"HowToStep",
+                        "position": "1",
+      "itemListElement":
+      [
+      {
+        "@type":"HowToDirection",
+                                "position": "1",
+        "description":"Install Yoast SEO and activate your Google Search Console.",
+        "duringMedia":
+        {
+        "@type":"ImageObject",
+        "contentUrl":"yoast_seo_search_console.jpg"
+        }
+      },
+      {
+        "@type":"HowToTip",
+                                "position": "2",
+        "description":"Did you know you can check and fix crawl errors directly from Yoast SEO?"
+      }
+      ]
+    },
+    {
+    }
+    ]
+  },
+  {
+    "@type":"HowToSection",
+    "name":"Adding your site to Search Console",
+                "position": "2",
+    "itemListElement":
+    [
+    {
+      "@type":"HowToStep",
+                        "position": "1",
+      "itemListElement":
+      {
+      "@type":"HowToDirection",
+                        "position": "1",
+      "description":"Go to Google Search Console (former Google Webmaster Tools), sign into your Google account and click the red button to add your website."
+      }
+    },
+    {
+      "@type":"HowToStep",
+                        "position": "2",
+      "itemListElement":
+      [
+      {
+        "@type":"HowToDirection",
+                                "position": "1",
+        "description":"Copy the code for the HTML tag under the Alternate Methods tab.",
+        "duringMedia":
+        {
+        "@type":"ImageObject",
+        "contentUrl":"yoast_seo_search_console_2.jpg"
+        }
+      },
+      {
+        "@type":"HowToTip",
+                                "position": "2",
+        "description":"Please make sure you enter your complete url."
+      }
+      ]
+    },
+    {
+      "@type":"HowToStep",
+                        "position": "3",
+      "itemListElement":
+      {
+      "@type":"HowToDirection",
+                        "position": "1",
+      "description":"Copy the code for the HTML tag under the Alternate Methods tab."
+      }
+    },
+    {
+      "@type":"HowToStep",
+                        "position": "4",
+      "itemListElement":
+      {
+      "@type":"HowToDirection",
+                        "position": "1",
+      "description":"Log in to your WordPress website and click on ‘SEO’ in your menu. After that, click on General."
+      }
+    },
+    {
+      "@type":"HowToStep",
+                        "position": "5",
+      "itemListElement":
+      [
+      {
+        "@type":"HowToDirection",
+                                "position": "1",
+        "description":"Click on the ‘Webmaster Tools’ tab and add the code under ‘Google Search Console’. Click ‘Save Changes’."
+      },
+        {
+      "@type":"HowToStep",
+                        "position": "6",
+      "itemListElement":
+      {
+      "@type":"HowToDirection",
+                        "position": "1",
+      "description":"Switch back to Google Search Console (formerly Google Webmaster Tools) and click ‘Verify’."
+      }
+    }
+      ]
+    }
+    ]
+  },
+  {
+    "@type":"HowToSection",
+    "name":"Finishing up",
+                "position": "3",
+    "itemListElement":
+    [
+    {
+      "@type":"HowToStep",
+                        "position": "1",
+      "itemListElement":
+      {
+      "@type":"HowToDirection",
+                        "position": "1",
+      "description":"Congratulations! You’ve connected your website to Google Search Console (former Google Webmaster Tools)!"
+      }
+    },
+    {
+      "@type":"HowToStep",
+                        "position": "2",
+      "itemListElement":
+      {
+      "@type":"HowToDirection",
+                        "position": "2",
+      "description":"Now that you’ve verified and connected your website, you can submit your sitemap!"
+      }
+    }
+    ]
+  }
+  ]
+}
+=========================
+*/
+?>
