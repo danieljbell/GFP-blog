13242bc1d7d3415b08a356b55e04a1bc207fefdb
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-11-05 08:37:21 -0500

    about us

--- wp-content/themes/gfp/page-templates/about-us.php
+++ wp-content/themes/gfp/page-templates/about-us.php
@@ -8,6 +8,47 @@ Template Name: About Us
 
 <?php get_header(); ?>
 
-asdf
+<section class="hero">
+  <div class="site-width">
+    <h1>About Green Farm Parts</h1>
+  </div>
+</section>
+
+<section class="pad-y--most">
+  <div class="site-width">
+    <h2 class="has-brand-two-text">Green Farm Parts: If it’s Green, We’ve Got it.</h2>
+    <p class="mar-b">Welcome to Green Farm Parts - your online home for John Deere parts, accessories and lifestyle products. Like you, we have a passion for Green. It’s who we are, and it’s who we’ve been for more than 60 years. Green Farm Parts is the online parts operation of Reynolds Farm Equipment, a regional John Deere retailer headquartered in Indiana.</p>
+    <p class="mar-b">Our mission at Green Farm Parts is simple – make it easy to get John Deere parts and accessories fast with an unparalleled approach to customer service. We love what we do, and we look forward to serving you soon.</p>
+  </div>
+</section>
+
+<section class="pad-y--most">
+  <div class="site-width">
+    <h2>Meet the Team</h2>
+    <ul class="about-us--team-members">
+      <li class="about-us--team-member">
+        <img src="//unsplash.it/225/275" alt="Bill Murray" class="team-member--headshot">
+        <div class="team-member--details">
+          <p class="team-member--name">Jon Schwander</p>
+          <p class="team-member--title">Full Stack Web Developer</p>
+        </div>
+      </li>
+      <li class="about-us--team-member">
+        <img src="//unsplash.it/225/275" alt="Bill Murray" class="team-member--headshot">
+        <div class="team-member--details">
+          <p class="team-member--name">Daniel Bell</p>
+          <p class="team-member--title">Full Stack Web Developer</p>
+        </div>
+      </li>
+      <li class="about-us--team-member">
+        <img src="//unsplash.it/225/275" alt="Bill Murray" class="team-member--headshot">
+        <div class="team-member--details">
+          <p class="team-member--name">Jason Imel</p>
+          <p class="team-member--title">Full Stack Web Developer</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</section>
 
 <?php get_footer(); ?>
