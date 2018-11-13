8989d55cec90a7059e7d4ae86d2291cb8eb0e45a
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-11-13 17:00:03 -0500

    helper function

--- wp-content/themes/gfp/src/js/modules/helpers.js
+++ wp-content/themes/gfp/src/js/modules/helpers.js
@@ -7,3 +7,9 @@ function getParameterByName(name, url) {
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 };
+
+var updateFullYear = document.querySelectorAll('.utility--updateDate');
+updateFullYear.forEach(function(text) {
+  var newDate = new Date();
+  text.textContent = newDate.getFullYear();
+});
