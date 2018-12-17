2164ae6523d8322a799568d45fb8730097761c93
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-12-17 13:07:01 -0500

    commit

--- wp-content/themes/gfp/functions.php
+++ wp-content/themes/gfp/functions.php
@@ -803,11 +803,11 @@ function send_order_comment() {
 
 
   
-function remove_output_structured_data() { 
+// function remove_output_structured_data() { 
   // remove_action( 'wp_footer', array( WC()->structured_data, 'output_structured_data' ), 10 ); // Frontend pages 
-  remove_action( 'woocommerce_email_order_details', array( WC()->structured_data, 'output_email_structured_data' ), 30 ); // Emails 
-} 
-add_action( 'init', 'remove_output_structured_data' );
+  // remove_action( 'woocommerce_email_order_details', array( WC()->structured_data, 'output_email_structured_data' ), 30 ); // Emails 
+// } 
+// add_action( 'init', 'remove_output_structured_data' );
 
 
 
