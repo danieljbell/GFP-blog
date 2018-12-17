6d46d119817c9f09f4f140e3178d9a17ce92e996
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-12-17 13:13:55 -0500

    add things back

--- wp-content/themes/gfp/functions.php
+++ wp-content/themes/gfp/functions.php
@@ -18,13 +18,13 @@ ADD GLOBAL JS TO PAGE
 function enqueue_global_js() {
   wp_enqueue_script('global', get_stylesheet_directory_URI() . '/dist/js/global.js', array(), '0.1.29', true);
 
-  // if (is_page_template( 'page-templates/check-order-status.php' ) || is_account_page()) {
-  //   $translation_array = array(
-  //     'ajax_url'   => admin_url( 'admin-ajax.php' ),
-  //     'nonce'  => wp_create_nonce( 'nonce_name' )
-  //   );
-  //   wp_localize_script( 'global', 'ajax_order_tracking', $translation_array );
-  // }
+  if (is_page_template( 'page-templates/check-order-status.php' ) || is_account_page()) {
+    $translation_array = array(
+      'ajax_url'   => admin_url( 'admin-ajax.php' ),
+      'nonce'  => wp_create_nonce( 'nonce_name' )
+    );
+    wp_localize_script( 'global', 'ajax_order_tracking', $translation_array );
+  }
   
 }
 add_action('wp_enqueue_scripts', 'enqueue_global_js');
@@ -803,11 +803,11 @@ function send_order_comment() {
 
 
   
-// function remove_output_structured_data() { 
-  // remove_action( 'wp_footer', array( WC()->structured_data, 'output_structured_data' ), 10 ); // Frontend pages 
-  // remove_action( 'woocommerce_email_order_details', array( WC()->structured_data, 'output_email_structured_data' ), 30 ); // Emails 
-// } 
-// add_action( 'init', 'remove_output_structured_data' );
+function remove_output_structured_data() { 
+  remove_action( 'wp_footer', array( WC()->structured_data, 'output_structured_data' ), 10 ); // Frontend pages 
+  remove_action( 'woocommerce_email_order_details', array( WC()->structured_data, 'output_email_structured_data' ), 30 ); // Emails 
+} 
+add_action( 'init', 'remove_output_structured_data' );
 
 
 
