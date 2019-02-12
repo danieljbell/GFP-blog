f754535bcd063a9fe39aeb9e0b6494b48dadf728
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2019-02-12 11:15:07 -0500

    init model selector

---
+++ wp-content/themes/gfp/page-templates/model-selector.php
@@ -0,0 +1,135 @@
+<?php 
+/*
+=============================
+Template Name: Model Selector
+=============================
+*/
+?>
+
+<?php get_header(); ?>
+
+<section class="hero">
+  <div class="site-width">
+    <h1>Choose Your Model</h1>
+  </div>
+</section>
+
+<section>
+  <div class="site-width">
+    <div class="all-models-container">
+      <div>
+        <section id="lawn-garden" class="mar-b--most">
+          <div class="box--with-header">
+            <header>
+              <h2>Lawn &amp; Garden</h2>
+            </header>
+            <?php
+              format_equipment_menu('lawn-garden', 'Lawn Tractors', 'lawn-tractors', 'lawn_tractors');
+              format_equipment_menu('lawn-garden', 'Zero Turns', 'zero-turns', 'zero_turns');
+              format_equipment_menu('lawn-garden', 'Compact Tractors', 'compact-tractors', 'compact_tractors');
+              format_equipment_menu('lawn-garden', 'Gators', 'gators', 'gators');
+              format_equipment_menu('lawn-garden', 'Walk Behind', 'walk-behind-mowers', 'walk_behind_mowers');
+              format_equipment_menu('lawn-garden', 'Hand Held Equipment', 'hand-held-equipment', 'hand_held_equipment');
+            ?>
+          </div>
+        </section>
+        <section id="agriculture" class="mar-b--most">
+          <div class="box--with-header">
+            <header>
+              <h2>Agriculture</h2>
+            </header>            
+            <?php
+              format_equipment_menu('agriculture', 'Sprayers', 'sprayers', 'sprayers');
+              format_equipment_menu('agriculture', 'Tractors', 'tractors', 'tractors');
+              format_equipment_menu('agriculture', 'Combines', 'combines', 'combines');
+              format_equipment_menu('agriculture', 'Balers', 'balers', 'balers');
+              format_equipment_menu('agriculture', 'Loaders', 'loaders', 'loaders');
+              format_equipment_menu('agriculture', 'Windrowers', 'windrowers', 'windrowers');
+            ?>
+          </div>
+        </section>
+        <section id="landscapers" class="mar-b--most">
+          <div class="box--with-header">
+            <header>
+              <h2>Landscapers</h2>
+            </header>
+            <?php
+              format_equipment_menu('landscapers', 'Zero Turns', 'zero-turn-mowers', 'zero_turn_mowers');
+              format_equipment_menu('landscapers', 'Front Mowers', 'front-mowers', 'front_mowers');
+              format_equipment_menu('landscapers', 'Quik-Traks', 'quik-traks', 'quik_traks');
+              format_equipment_menu('landscapers', 'Walk Behind', 'walk-behind-mowers', 'walk_behind_mowers');
+              format_equipment_menu('landscapers', 'Wide Area', 'wide-area-mowers', 'wide_area_mowers');
+            ?>
+          </div>
+        </section>
+        <section id="golf">
+          <div class="box--with-header">
+            <header>
+              <h2>Golf</h2>
+            </header>
+            <?php
+              format_equipment_menu('golf', 'Aerators', 'aeration', 'aeration');
+              format_equipment_menu('golf', 'Greens Mowers', 'greens-mowers', 'greens_mowers');
+              format_equipment_menu('golf', 'Fairway Mowers', 'fairway-mowers', 'fairway_mowers');
+              format_equipment_menu('golf', 'Rough & Trim Mowers', 'rough-trim-mowers', 'rough_trim_mowers');
+              format_equipment_menu('golf', 'Turf Sprayers', 'turf-sprayers', 'turf_sprayers');
+            ?>
+          </div>
+        </section>
+      </div>
+      <aside>
+        <div class="sticky-elements">
+          <div class="form-group mar-b--more">
+            <label for="search_model" class="visually-hidden">Filter Page By Model Number</label>
+            <input type="text" name="search_model" id="search_model" placeholder="Filter Page By Model Number">
+          </div>
+          <div class="box--with-header">
+            <header>Categories</header>
+            <ul>
+              <li><a href="/model-selector/#lawn-garden">Lawn &amp; Garden</a></li>
+              <li><a href="/model-selector/#agriculture">Agriculture</a></li>
+              <li><a href="/model-selector/#landscapers">Landscapers</a></li>
+              <li><a href="/model-selector/#golf">Golf</a></li>
+            </ul>
+          </div>
+        </div>
+      </aside>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
+
+<?php 
+function format_equipment_menu($parent, $pretty_name, $slug, $equip_var) {
+    $equipment_query = new WP_Query(array(
+      'post_type' => 'post',
+      'category' => 'maintenance-reminder',
+      'posts_per_page' => -1,
+      'tag_slug__and' => [$parent, $slug]
+    ));
+    $posts = [];
+    if ($equipment_query->have_posts()) : while ($equipment_query->have_posts()) : $equipment_query->the_post();
+      $name = get_the_title();
+      $name = str_replace("John Deere", "", $name);
+      $name = str_replace("Maintenance Guide", "", $name);
+      array_push($posts, array(
+        'name' => $name,
+        'link' => get_the_permalink()
+      ));
+    endwhile; endif; wp_reset_query();
+    echo '<div class="industry-vertical--section" id="' . $parent . '-' . $slug . '">';
+      echo '<div class="industry-vertical--image">';
+        echo '<img src="' . get_stylesheet_directory_URI() . '/dist/img/' . $parent . '-' . $slug . '.jpg" alt="' . $pretty_name . '">';
+      echo '</div>';
+      echo '<div class="industry-vertical--links">';
+        echo '<h3>' . $pretty_name . '</h3>';
+        echo '<ul>';
+          foreach ($posts as $key => $post) {
+            print_r($post);
+            echo '<br />';
+          }
+        echo '</ul>';
+      echo '</div>';  
+    echo '</div>';
+  }
--- wp-content/themes/gfp/partials/display-site-header.php
+++ wp-content/themes/gfp/partials/display-site-header.php
@@ -11,155 +11,22 @@
         <li class="mega-menu">
           <button class="navigation--button">Shop By Part</button>
           <?php
-
             wp_nav_menu( array(
               'menu' => 'shop-by-part',
               'menu_class' => 'mega-menu--list mega-menu--shop-by-part',
               'container' => ''
             ) );
-
-            // $taxonomy = 'product_cat';
-            // $orderby = 'name';
-
-            // $args = array(
-            //   'taxonomy'     => $taxonomy,
-            //   'hierarchical' => $hierarchical,
-            //   'orderby'      => $orderby,
-            //   'order'        => 'DESC',
-            //   'title_li'     => $title,
-            //   'hide_empty'   => false,
-            //   'exclude'      => array(715, 820)
-            // );
-
-            // $all_categories = get_categories( $args );
-
-            // echo '<ul class="mega-menu--list mega-menu--shop-by-part">';
-              
-            //   $i = 0;
-            //   foreach ($all_categories as $cat) {
-                
-            //     if ($cat->category_parent == 0) {
-            //       $category_id = $cat->term_id;       
-            //       if ($i === 0) {
-            //         echo '<li class="mega-menu--parent"><a href="'. get_term_link($cat->slug, 'product_cat') .'">'. $cat->name .'</a>';
-            //       } else {
-            //         echo '<li class="mega-menu--parent mega-menu--parent--is-hidden"><a href="'. get_term_link($cat->slug, 'product_cat') .'">'. $cat->name .'</a>';
-            //       }
-
-            //         $args2 = array(
-            //           'taxonomy'     => $taxonomy,
-            //           'child_of'     => 0,
-            //           'parent'       => $category_id,
-            //           'orderby'      => $orderby,
-            //           'show_count'   => $show_count,
-            //           'pad_counts'   => $pad_counts,
-            //           'hierarchical' => $hierarchical,
-            //           'title_li'     => $title,
-            //           'hide_empty'   => $empty
-            //         );
-                    
-            //         $sub_cats = get_categories( $args2 );
-            //         echo '<ul class="mega-menu--child-list">';
-            //           if ($sub_cats) {
-            //             foreach($sub_cats as $sub_category) {
-            //                 $thumbnail_id = get_woocommerce_term_meta( $sub_category->term_id, 'thumbnail_id', true );
-            //                 $image = wp_get_attachment_url( $thumbnail_id );
-            //               echo  '<li class="mega-menu--child-item"><a href="'. get_term_link($sub_category->slug, 'product_cat') .'">';
-            //                 echo '<img src="' . $image . '" class="mega-menu--item-image">';
-            //                 echo $sub_category->name;
-            //               echo '</a></li>';
-            //             }   
-            //             // echo '<li><a href="' . get_term_link($cat->slug, 'product_cat') . '">Shop All ' . number_format($cat->category_count, null, '', ',') . ' ' . $cat->name . '</a></li>';
-            //           } else {
-            //             // echo '<li><a href="' . get_term_link($cat->slug, 'product_cat') . '">Shop All ' . number_format($cat->category_count, null, '', ',') . ' ' . $cat->name . '</a></li>';
-            //           }
-            //         echo '</ul>';
-
-            //       echo '</li>';
-            //       $i++;
-            //     }
-
-
-            //   }
-
-            // echo '</ul>';
-
           ?>
         </li>
         <li class="mega-menu">
           <button class="navigation--button">Shop By Equipment</button>
-          <ul class="mega-menu--list mega-menu--shop-by-equipment">
-            <li class="mega-menu--parent menu-item-has-children">
-              <a href="#0">Lawn & Garden</a>
-              <ul class="mega-menu--child-list">
-                <?php
-                  // format_equipment_menu('lawn-garden', 'Lawn Tractors', 'lawn-tractors', 'lawn_tractors');
-                  // format_equipment_menu('lawn-garden', 'Zero Turns', 'zero-turns', 'zero_turns');
-                  // format_equipment_menu('lawn-garden', 'Compact Tractors', 'compact-tractors', 'compact_tractors');
-                  // format_equipment_menu('lawn-garden', 'Gators', 'gators', 'gators');
-                  // format_equipment_menu('lawn-garden', 'Walk Behind', 'walk-behind-mowers', 'walk_behind_mowers');
-                  // format_equipment_menu('lawn-garden', 'Front Mowers', 'front-mowers', 'front_mowers');
-                  // format_equipment_menu('lawn-garden', 'Hand Held Equipment', 'hand-held-equipment', 'hand_held_equipment');
-                  // format_equipment_menu('lawn-garden', 'Loaders', 'loaders', 'loaders');
-                  // format_equipment_menu('lawn-garden', 'Snow Blowers', 'snow-blower', 'snow_blower');
-                ?>
-              </ul>
-            </li>
-            <li class="mega-menu--parent mega-menu--parent--is-hidden menu-item-has-children">
-              <a href="">Agriculture</a>
-              <ul class="mega-menu--child-list">
-                <?php
-                  // format_equipment_menu('agriculture', 'Sprayers', 'sprayers', 'sprayers');
-                  // format_equipment_menu('agriculture', 'Tractors', 'tractors', 'tractors');
-                  // format_equipment_menu('agriculture', 'Combines', 'combines', 'combines');
-                  // format_equipment_menu('agriculture', 'Balers', 'balers', 'balers');
-                  // format_equipment_menu('agriculture', 'Loaders', 'loaders', 'loaders');
-                  // format_equipment_menu('agriculture', 'Windrowers', 'windrowers', 'windrowers');
-                  // format_equipment_menu('agriculture', 'Planters', 'planter', 'planter');
-                  // format_equipment_menu('agriculture', 'Rotary Cutters', 'rotary-cutter', 'rotary_cutter');
-                ?>
-              </ul>
-            </li>
-            <li class="mega-menu--parent mega-menu--parent--is-hidden menu-item-has-children">
-              <a href="">Landscapers</a>
-              <ul class="mega-menu--child-list">
-                <?php
-                  // format_equipment_menu('landscapers', 'Zero Turns', 'zero-turn-mowers', 'zero_turn_mowers');
-                  // format_equipment_menu('landscapers', 'Front Mowers', 'front-mowers', 'front_mowers');
-                  // format_equipment_menu('landscapers', 'Quik-Traks', 'quik-traks', 'quik_traks');
-                  // format_equipment_menu('landscapers', 'Walk Behind', 'walk-behind-mowers', 'walk_behind_mowers');
-                  // format_equipment_menu('landscapers', 'Wide Area', 'wide-area-mowers', 'wide_area_mowers');
-                  // format_equipment_menu('landscapers', 'Loaders', 'loaders', 'loaders');
-                ?>
-              </ul>
-            </li>
-            <li class="mega-menu--parent mega-menu--parent--is-hidden menu-item-has-children">
-              <a href="">Golf</a>
-              <ul class="mega-menu--child-list">
-                <?php
-                  // format_equipment_menu('golf', 'Aerators', 'aeration', 'aeration');
-                  // format_equipment_menu('golf', 'Greens Mowers', 'greens-mowers', 'greens_mowers');
-                  // format_equipment_menu('golf', 'Fairway Mowers', 'fairway-mowers', 'fairway_mowers');
-                  // format_equipment_menu('golf', 'Rough & Trim Mowers', 'rough-trim-mowers', 'rough_trim_mowers');
-                  // format_equipment_menu('golf', 'Turf Sprayers', 'turf-sprayers', 'turf_sprayers');
-                ?>
-              </ul>
-            </li>
-            <?php
-            /*
-            =========================
-            <li class="mega-menu--parent mega-menu--parent--is-hidden">
-              <a href="">Construction</a>
-              <ul class="mega-menu--child-list">
-                <?php
-                  format_equipment_menu('construction', 'Undercarraige', 'undercarraige', 'undercarraige');
-                ?>
-              </ul>
-            </li>
-            =========================
-            */
-            ?>
-          </ul>
+          <?php
+            wp_nav_menu( array(
+              'menu' => 'shop-by-equipment',
+              'menu_class' => 'mega-menu--list mega-menu--shop-by-part',
+              'container' => ''
+            ) );
+          ?>
         </li>
         <li class="mega-menu">
           <button class="navigation--button">Shop By Brand</button>
@@ -209,35 +76,3 @@
     </div>
   </div>
 </header>
-
-<?php
-  function format_equipment_menu($parent, $pretty_name, $slug, $equip_var) {
-    $query = new WP_Query(array(
-      'post_type' => 'post',
-      'category' => 'maintenance-reminder',
-      'posts_per_page' => -1,
-      'tag_slug__and' => [$parent, $slug]
-    ));
-    $posts = [];
-    if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
-      $name = get_the_title();
-      $name = str_replace("John Deere", "", $name);
-      $name = str_replace("Maintenance Guide", "", $name);
-      array_push($posts, array(
-        'name' => $name,
-        'link' => get_the_permalink()
-      ));
-    endwhile; endif; wp_reset_query();
-    echo '<li class="mega-menu--child-item">';
-      echo '<button class="mega-menu--equipment-parent">';
-        echo '<img src="' . get_stylesheet_directory_URI() . '/dist/img/' . $parent . '-' . $slug . '.jpg" alt="' . $pretty_name . '">' . $pretty_name;
-      echo '</button>';
-      echo '<ul class="visually-hidden">';
-        foreach ($posts as $key => $equip) {
-          echo '<li><a href="' . $equip['link'] . '" style="padding: 0; display: inline-block; text-align: left;">' . $equip['name'] . '</a></li>';
-        }
-      echo '</ul>';
-    echo '</li>';
-  }
-?>
-
