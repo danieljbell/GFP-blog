1c394af2aa468082ad6bc16ec23ecc92eb5de075
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2019-02-13 14:35:15 -0500

    another approach to ajax

--- wp-content/themes/gfp/partials/display-maintenance-reminder.php
+++ wp-content/themes/gfp/partials/display-maintenance-reminder.php
@@ -299,7 +299,7 @@ foreach (get_the_tags() as $tag) {
                   <tr>
                     <?php
                       $sku = get_sub_field($section . '_number');
-                      $sku = preg_replace("/\s|&nbsp;/", '', $sku);
+                      $sku = preg_replace("/\s/", '', $sku);
                     ?>
                     <td data-header="Part" data-sku="<?php echo strtoupper($sku); ?>">
                       <?php if ($interval && get_sub_field('quantity')) : ?>
