aeda8a09dcaca7266f864effd82de0da6fd9b7ee
Daniel Bell <dbell@w039606523.rfe.local>
2019-02-20 22:15:30 -0500

    adding parts for admin order

--- wp-content/themes/gfp/page-templates/admin-phone-order.php
+++ wp-content/themes/gfp/page-templates/admin-phone-order.php
@@ -15,7 +15,7 @@ Template Name: Admin Phone Order
     </div>
   </section>
 
-  <section>
+  <section class="mar-b--most">
     <div class="site-width">
       <div class="box--with-header">
         <header>Customer Details</header>
@@ -25,12 +25,66 @@ Template Name: Admin Phone Order
           <div class="customer--returning">
             <h3>Returning Customer</h3>
             <input id="returning_email_address" type="email" placeholder="Search by email address" name="returning_email_address" style="min-width: 300px;">
-            <ul class="returning-customer-list"></ul>
+            <div class="returning-customer visually-hidden">
+              <div class="customer-meta mar-y">
+                <div class="display-name"></div>
+              </div>
+              <div class="customer-billing mar-b">
+                <h4>Billing</h4>
+                <address>
+                  <div class="street"></div>
+                  <div class="city"></div>
+                  <div class="zip"></div>
+                </address>
+              </div>
+              <div class="customer-shipping mar-b">
+                <h4>Shipping</h4>
+                <address>
+                  <div class="street"></div>
+                  <div class="city"></div>
+                  <div class="zip"></div>
+                </address>
+              </div>
+              <button class="btn-solid--brand" id="choose_customer">Choose Customer</button>
+            </div>
           </div>
           <div class="customer--new visually-hidden">
             <h3>New Customer</h3>
           </div>
         </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="site-width">
+      <div class="box--with-header">
+        <header>Line Items</header>
+        <form action="" id="addSkus">
+          <div class="grid--phone-half">
+            <div class="form-group has-text-right">
+              <label for="sku">Part Number</label>
+              <input type="text" name="sku" id="sku">
+            </div>
+            <div class="form-group">
+              <label for="qty">Quantity</label>
+              <input type="number" min="1" max="100" name="qty" id="qty" value="1">
+              <button class="btn-solid--brand mar-l">Add Part</button>
+            </div>
+          </div>
+        </form>
+        <table class="skus visually-hidden">
+          <thead>
+            <tr>
+              <th>Part Number</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Qty</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
       </div>
     </div>
   </section>
