20f9243d6d45f6e5ece8dcd014c19f519c5452d8
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2019-04-09 12:51:14 -0400

    change

--- wp-content/themes/gfp/src/js/modules/checkout.js
+++ wp-content/themes/gfp/src/js/modules/checkout.js
@@ -1,17 +1,17 @@
 (function($) {
   
-  var shipToDifferent = document.querySelector('#ship-to-different-address-checkbox');
-  var shipDetails = document.querySelector('.shipping-details--is-hidden');
+  // var shipToDifferent = document.querySelector('#ship-to-different-address-checkbox');
+  // var shipDetails = document.querySelector('.shipping-details--is-hidden');
   
-  if (shipToDifferent) {
-    shipToDifferent.addEventListener('change', function(e) {
-      if (!shipToDifferent.checked) {
-        shipDetails.classList.add('shipping-details--is-hidden');
-        return;
-      }
-      shipDetails.classList.remove('shipping-details--is-hidden');
-    });
-  }
+  // if (shipToDifferent) {
+  //   shipToDifferent.addEventListener('change', function(e) {
+  //     if (!shipToDifferent.checked) {
+  //       shipDetails.classList.add('shipping-details--is-hidden');
+  //       return;
+  //     }
+  //     shipDetails.classList.remove('shipping-details--is-hidden');
+  //   });
+  // }
 
   mailchimpOptIn = document.querySelector('#mailchimp_woocommerce_newsletter');
   if (mailchimpOptIn) {
