baf0bec95f706a167cf820f37b803f9bf2fbe81d
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2019-04-22 08:51:50 -0400

    shipping

--- wp-content/themes/gfp/woocommerce/checkout/form-shipping.php
+++ wp-content/themes/gfp/woocommerce/checkout/form-shipping.php
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
   <?php if ( true === WC()->cart->needs_shipping_address() ) : ?>
 
   <label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
-    <input id="ship-to-different-address-checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" <?php checked( apply_filters( 'woocommerce_ship_to_different_address_checked', 'shipping' === get_option( 'woocommerce_ship_to_destination' ) ? 1 : 0 ), 1 ); ?> type="checkbox" name="ship_to_different_address" value="1" /> <span><?php _e( 'Need to ship to a different address?', 'woocommerce' ); ?></span>
+    <input id="ship-to-different-address-checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" type="checkbox" name="ship_to_different_address" value="1" /> <span><?php _e( 'Need to ship to a different address?', 'woocommerce' ); ?></span>
   </label>
     
     <div class="shipping-details--container shipping-details--is-hidden">
