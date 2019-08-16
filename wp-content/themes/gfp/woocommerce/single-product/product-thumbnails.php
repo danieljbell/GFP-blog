0bacf1f836554e48b4a43d404b7f2161fda9a303
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2019-08-16 15:40:17 -0400

    image fix

--- wp-content/themes/gfp/woocommerce/single-product/product-thumbnails.php
+++ wp-content/themes/gfp/woocommerce/single-product/product-thumbnails.php
@@ -25,14 +25,15 @@ if ( ! function_exists( 'wc_get_gallery_image_html' ) ) {
 
 global $product;
 
+$post_thumbnail_id = $product->get_image_id();
 $attachment_ids = $product->get_gallery_image_ids();
 
 if ( $attachment_ids && has_post_thumbnail() ) {
   $i = 1;
   echo '<ul class="woocommerce-product-gallery__thumbs">';
-    echo '<li>' . apply_filters( 'woocommerce_single_product_image_thumbnail_html', '<img src="https://res.cloudinary.com/greenfarmparts/image/upload/w_100,h_100,c_fill/v1542129610/' . $product->get_sku() . '-0.jpg" data-full-image="https://res.cloudinary.com/greenfarmparts/image/upload/fl_tiled,l_overlay,o_10/' . $product->get_sku() . '-0.jpg" />', $attachment_id ) . '</li>';
+    echo '<li>' . apply_filters( 'woocommerce_single_product_image_thumbnail_html', '<img src="https://res.cloudinary.com/greenfarmparts/image/fetch/w_75,h_75,c_fill/' . str_replace('gfp.local', 'greenfarmparts.com', wp_get_attachment_image_url($post_thumbnail_id, 'full')) . '" data-full-image="https://res.cloudinary.com/greenfarmparts/fetch/fl_tiled,l_overlay,o_10/' . str_replace('gfp.local', 'greenfarmparts.com', wp_get_attachment_image_url($post_thumbnail_id, 'full')) . '" />', $post_thumbnail_id ) . '</li>';
     foreach ( $attachment_ids as $attachment_id ) {
-      echo '<li>' .apply_filters( 'woocommerce_single_product_image_thumbnail_html', '<img src="https://res.cloudinary.com/greenfarmparts/image/upload/w_100,h_100,c_fill/v1542129610/' . $product->get_sku() . '-' . $i . '.jpg" data-full-image="https://res.cloudinary.com/greenfarmparts/image/upload/fl_tiled,l_overlay,o_10/' . $product->get_sku() . '-' . $i . '.jpg" />', $attachment_id ) . '</li>';
+      echo '<li>' . apply_filters( 'woocommerce_single_product_image_thumbnail_html', '<img src="https://res.cloudinary.com/greenfarmparts/image/fetch/w_75,h_75,c_fill/' . str_replace('gfp.local', 'greenfarmparts.com', wp_get_attachment_image_url($attachment_id)) .'" data-full-image="' . str_replace('gfp.local', 'greenfarmparts.com', wp_get_attachment_image_url($attachment_id, 'full')) . '" />', $attachment_id ) . '</li>';
       $i++;
     }
   echo '</ul>';
