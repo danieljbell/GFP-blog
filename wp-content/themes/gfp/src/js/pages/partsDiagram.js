4c6665d4bc038dcc9baf4ce3e16575f3d135098a
Daniel Bell <dbell@w039606518.rfe.local>
2019-11-18 11:15:26 -0500

    get qty

--- wp-content/themes/gfp/src/js/pages/partsDiagram.js
+++ wp-content/themes/gfp/src/js/pages/partsDiagram.js
@@ -6,9 +6,17 @@
   document.body.addEventListener('click', sendToCart);
 
   function sendToCart(e) {
-    if (e.target.classList.contains('ariPartListAddToCart')) {
-      console.log('ARI add to cart');
+    if (!e.target.classList.contains('ariPartListAddToCart')) {
+      return;
     }
+
+    console.log('ARI add to cart');
+    var partQty = getQty(e.target);
+    console.log(partQty);
+  }
+
+  function getQty(target) {
+    return 'getting qty';
   }
 
 })();
